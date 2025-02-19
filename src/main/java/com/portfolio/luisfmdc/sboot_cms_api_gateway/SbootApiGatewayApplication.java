75f204d0e92c39dc205da11502e013a32edef70d
luisfmaiadc <lf.mcosta23@gmail.com>
2025-02-19 00:46:54 -0300

    Modificações no pom.xml e em Application.

--- src/main/java/com/portfolio/luisfmdc/sboot_cms_api_gateway/SbootApiGatewayApplication.java
+++ src/main/java/com/portfolio/luisfmdc/sboot_cms_api_gateway/SbootApiGatewayApplication.java
@@ -2,8 +2,9 @@ package com.portfolio.luisfmdc.sboot_cms_api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SbootApiGatewayApplication {
 
 	public static void main(String[] args) {
