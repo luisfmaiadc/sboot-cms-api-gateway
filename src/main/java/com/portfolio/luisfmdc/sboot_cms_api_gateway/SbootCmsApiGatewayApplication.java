77b215dd12d13ebd8e3085d8ca651e457352f383
luisfmaiadc <lf.mcosta23@gmail.com>
2025-02-18 23:53:17 -0300

    Inicialização do versionamento do projeto de API Gateway.

---
+++ src/main/java/com/portfolio/luisfmdc/sboot_cms_api_gateway/SbootCmsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sboot_cms_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootCmsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootCmsApiGatewayApplication.class, args);
+	}
+
+}
